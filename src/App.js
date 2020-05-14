f827dc950b402270afb3e728f9bc951821b45641
Riot Nicolas <nico.riot@free.fr>
2020-05-14 21:10:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>React  Frontend</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
