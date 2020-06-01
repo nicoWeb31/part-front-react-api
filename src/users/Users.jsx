b2877212e69997ca0298e7594c1c2db7ca316b9e
Riot Nicolas <nico.riot@free.fr>
2020-06-01 21:28:28 +0200

    user component

--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import {list} from './apiUser';
+import { list } from './apiUser';
+import User from './User'
+
 
 
 class Users extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users:''
+            users:[]
         };
     }
 
@@ -27,14 +29,30 @@ class Users extends Component {
     }
 
     render() {
+
+        const {users} = this.state
+
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Users List</h2>
-                
+                <div className="card">
+                    {this.state.users && users.map((user,id)=>
+                        
+                        <User data={user} key={id} />
+
+                    
+
+                    )}
+                </div>
             </div>
 
         );
     }
 }
+
+                        
+                    
+
+
 
 export default Users;
